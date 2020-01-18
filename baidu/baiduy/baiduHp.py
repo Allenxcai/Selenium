c3f3f7f4a184c597a2cf8fe5f71814a3e03bce13
AXC <1729011@qq.com>
2020-01-18 15:51:42 +0800

    自动化测试学习记录

--- baidu/baiduy/baiduHp.py
+++ baidu/baiduy/baiduHp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2020/1/18 15:31
 # @Author  : Allen.XC.AI
-# @File    : BaiduHp.py
+# @File    : baiduHp.py
 
 hello='hello world'
 def add(a,b):
@@ -10,5 +10,11 @@ def add(a,b):
 class index(object):
     def __init__(self):
         pass
-def add(self,a,b):
-    return a+b
+    def add(self,a,b):
+        return a+b
+
+if __name__=="__main__":
+    print(hello)
+    print(add(2,3))
+    demo=index()
+    print(demo.add(3,4))
---
+++ baidu/index/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/18 15:40
+# @Author  : Allen.XC.AI
+# @File    : index.py
+import baiduHp
+if __name__=="__main__":
+    print(BaiduHp.hello)
+    print(BaiduHp.add(2,3))
+    demo=BaiduHp.index()
+    print(demo.add(3,3))
